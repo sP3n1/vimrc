8f4e59412dde3f50c4fc8b1b99291854091bb26c
sP3n1 <sP3n1@users.noreply.github.com>
2022-09-07 15:23:11 +0200

    Include vim-ale in setup

--- setupvim.sh
+++ setupvim.sh
@@ -6,14 +6,17 @@ cd
 wget "https://raw.githubusercontent.com/sP3n1/vimrc/master/.vimrc"
 
 # install nerdtree
-git clone --depth=1 https://github.com/preservim/nerdtree.git ~/.vim/pack/vendor/start/nerdtree
-< /dev/tty vim -u NONE -c "helptags ~/.vim/pack/vendor/start/nerdtree/doc" -c q
+git clone --depth=1 https://github.com/preservim/nerdtree.git ~/.vim/pack/git-plugins/start/nerdtree
+< /dev/tty vim -u NONE -c "helptags ~/.vim/pack/git-plugins/start/nerdtree/doc" -c q
 # install vim-airline
-git clone --depth=1 https://github.com/vim-airline/vim-airline ~/.vim/pack/vendor/start/vim-airline
-< /dev/tty vim -u NONE -c "helptags ~/.vim/pack/vendor/start/vim-airline/doc" -c q
+git clone --depth=1 https://github.com/vim-airline/vim-airline ~/.vim/pack/git-plugins/start/vim-airline
+< /dev/tty vim -u NONE -c "helptags ~/.vim/pack/git-plugins/start/vim-airline/doc" -c q
 # install vim-fugitive
-git clone --depth=1 https://tpope.io/vim/fugitive.git ~/.vim/pack/vendor/start/vim-fugitive
-< /dev/tty vim -u NONE -c "helptags ~/.vim/pack/vendor/start/vim-fugitive/doc" -c q
+git clone --depth=1 https://tpope.io/vim/fugitive.git ~/.vim/pack/git-plugins/start/vim-fugitive
+< /dev/tty vim -u NONE -c "helptags ~/.vim/pack/git-plugins/start/vim-fugitive/doc" -c q
+# install vim-ale
+git clone --depth 1 https://github.com/dense-analysis/ale.git ~/.vim/pack/git-plugins/start/ale
+< /dev/tty vim -u NONE -c "helptags ~/.vim/pack/git-plugins/start/vim-ale/doc" -c q
 
 echo
 echo "[+] vim is configured and ready to use \\o/"

78eebe4453994c4ec7f2797f85b24ed9e851e058
sP3n1 <sP3n1@users.noreply.github.com>
2022-09-07 12:50:44 +0200

    Add more plugins to vim

--- setupvim.sh
+++ setupvim.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
-set -e
-mkdir ~/.vim ~/.vim/plugin ~/.vim/doc ~/.vim/backups ~/.vim/undo ~/.vim/swaps
+set -euo pipefail
+
+mkdir -p ~/.vim/plugin ~/.vim/doc ~/.vim/backups ~/.vim/undo ~/.vim/swaps
 cd
 wget "https://raw.githubusercontent.com/sP3n1/vimrc/master/.vimrc"
-cd ~/.vim
-wget "https://www.vim.org/scripts/download_script.php?src_id=23731" -O nerdtree.zip
-unzip nerdtree.zip
-rm nerdtree.zip
+
+# install nerdtree
+git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/vendor/start/nerdtree
+vim -u NONE -c "helptags ~/.vim/pack/vendor/start/nerdtree/doc" -c q
+# install vim-airline
+git clone https://github.com/vim-airline/vim-airline ~/.vim/pack/vendor/start/vim-airline
+vim -u NONE -c "helptags ~/.vim/pack/vendor/start/vim-airline/doc" -c q
+# install vim-fugitive
+git clone https://tpope.io/vim/fugitive.git ~/.vim/pack/vendor/start/vim-fugitive
+vim -u NONE -c "helptags ~/.vim/pack/vendor/start/vim-fugitive/doc" -c q
+
 echo
 echo "[+] vim is configured and ready to use \\o/"

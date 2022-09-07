ee8612ff5875cec6200e31d65351ae178eab12ec
sP3n1 <sP3n1@users.noreply.github.com>
2022-09-07 12:55:32 +0200

    Attempt to fix vim warning

--- setupvim.sh
+++ setupvim.sh
@@ -7,13 +7,13 @@ wget "https://raw.githubusercontent.com/sP3n1/vimrc/master/.vimrc"
 
 # install nerdtree
 git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/vendor/start/nerdtree
-vim -u NONE -c "helptags ~/.vim/pack/vendor/start/nerdtree/doc" -c q
+< /dev/tty vim -u NONE -c "helptags ~/.vim/pack/vendor/start/nerdtree/doc" -c q
 # install vim-airline
 git clone https://github.com/vim-airline/vim-airline ~/.vim/pack/vendor/start/vim-airline
-vim -u NONE -c "helptags ~/.vim/pack/vendor/start/vim-airline/doc" -c q
+< /dev/tty vim -u NONE -c "helptags ~/.vim/pack/vendor/start/vim-airline/doc" -c q
 # install vim-fugitive
 git clone https://tpope.io/vim/fugitive.git ~/.vim/pack/vendor/start/vim-fugitive
-vim -u NONE -c "helptags ~/.vim/pack/vendor/start/vim-fugitive/doc" -c q
+< /dev/tty vim -u NONE -c "helptags ~/.vim/pack/vendor/start/vim-fugitive/doc" -c q
 
 echo
 echo "[+] vim is configured and ready to use \\o/"

bfd2c31a5b31f20bd9210a0aa0a9dbd8d7407933
sP3n1 <sP3n1@users.noreply.github.com>
2022-09-07 13:15:05 +0200

    Improve speed downloading a shallow copy of plugins

--- setupvim.sh
+++ setupvim.sh
@@ -6,13 +6,13 @@ cd
 wget "https://raw.githubusercontent.com/sP3n1/vimrc/master/.vimrc"
 
 # install nerdtree
-git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/vendor/start/nerdtree
+git clone --depth=1 https://github.com/preservim/nerdtree.git ~/.vim/pack/vendor/start/nerdtree
 < /dev/tty vim -u NONE -c "helptags ~/.vim/pack/vendor/start/nerdtree/doc" -c q
 # install vim-airline
-git clone https://github.com/vim-airline/vim-airline ~/.vim/pack/vendor/start/vim-airline
+git clone --depth=1 https://github.com/vim-airline/vim-airline ~/.vim/pack/vendor/start/vim-airline
 < /dev/tty vim -u NONE -c "helptags ~/.vim/pack/vendor/start/vim-airline/doc" -c q
 # install vim-fugitive
-git clone https://tpope.io/vim/fugitive.git ~/.vim/pack/vendor/start/vim-fugitive
+git clone --depth=1 https://tpope.io/vim/fugitive.git ~/.vim/pack/vendor/start/vim-fugitive
 < /dev/tty vim -u NONE -c "helptags ~/.vim/pack/vendor/start/vim-fugitive/doc" -c q
 
 echo
